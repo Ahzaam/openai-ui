102bc71efad1e363eadd39742c5b1c919cda5e1c
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-03-22 16:40:58 +0530

    changes

--- src/pages/generate.jsx
+++ src/pages/generate.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export default function getCaption(){
     axios.post('https://openai-fiver.vercel.app/', {
-        keywords: ['caption', '
+        keywords: 'ahzam, pilot'
+    }).then((response) => {
+        console.log(response);
     })
 
 }
