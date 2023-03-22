271d1717036013d39f10bc7ba003a9db67fcb7e8
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-03-22 16:07:56 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <ProductDisplay />
     </div>
   );
 }
 
 export default App;
+
+const ProductDisplay = () => (
+  <section>
+    <div className="product">
+      <div className="description">
+        <h3>Starter plan</h3>
+        <h5>$20.00 / month</h5>
+      </div>
+    </div>
+    <form action="/create-checkout-session" method="POST">
+      {/* Add a hidden field with the lookup_key of your Price */}
+      <input type="hidden" name="lookup_key" value="{{PRICE_LOOKUP_KEY}}" />
+      <button id="checkout-and-portal-button" type="submit">
+        Checkout
+      </button>
+    </form>
+  </section>
+);
+
+const SuccessDisplay = ({ sessionId }) => {
+  return (
+    <section>
+      <div className="product Box-root">
+        <div className="description Box-root">
+          <h3>Subscription to starter plan successful!</h3>
+        </div>
+      </div>
+      <form action="/create-portal-session" method="POST">
+        <input
+          type="hidden"
+          id="session-id"
+          name="session_id"
+          value={sessionId}
+        />
+        <button id="checkout-and-portal-button" type="submit">
+          Manage your billing information
+        </button>
+      </form>
+    </section>
+  );
+};
+
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
