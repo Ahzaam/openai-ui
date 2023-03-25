57924553695d41ece4bf71f61bf95a5e0a7e4c6d
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-03-25 15:05:20 +0530

    dfghj

--- src/pages/authentication.jsx
+++ src/pages/authentication.jsx
@@ -31,11 +31,13 @@ export default function Authentication() {
     return (
         <>
 
+            <div className="flex items-center justify-center" style={{height:'80vh'}}>
             <button
                 onClick={google_auth}
                 type="submit" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-800">
                 Google Login
             </button>
+            </div>
 
         </>
     )
