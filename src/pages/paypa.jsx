95f5ba913eb4a428e327b28017f9756ff783f1e6
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-03-28 17:17:43 +0530

    paypal

--- src/pages/paypa.jsx
+++ src/pages/paypa.jsx
@@ -1,17 +1,14 @@
 // App.js
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-export default function Paypal() {
-  const initialOptions = {
-    "client-id":
-      "AR6IrZFZFKrlig1Dm3BQrsgm0nLz_6Iy7_7ROaWO_n0b__a_GW0eiz1OUUjmjK8ALqnk21pLEBn0-YQk",
-    currency: "USD",
-    intent: "capture",
-    "data-client-token": "abc123xyz==",
-  };
-
+export default function App() {
   return (
-    <PayPalScriptProvider options={initialOptions} deferLoading={true}>
+    <PayPalScriptProvider
+      options={{
+        "client-id":
+          "AR6IrZFZFKrlig1Dm3BQrsgm0nLz_6Iy7_7ROaWO_n0b__a_GW0eiz1OUUjmjK8ALqnk21pLEBn0-YQk",
+      }}
+    >
       <PayPalButtons style={{ layout: "horizontal" }} />
     </PayPalScriptProvider>
   );
